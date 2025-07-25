72e0c2eedec70bbae98119f71a45d5fc14bda8b6
Govinda <fearlessjack00@gmail.com>
2025-07-25 23:37:03 +0530

    hi

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,13 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 // app.use(cors());
-app.use(cors({
+const corsOptions = {
     origin: '*',
-}));
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
 app.get('/', (req, res) => {
     res.send('Reyansh Backend API Running');
 });
